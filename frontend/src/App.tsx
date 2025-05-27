5430b4437f795f0b136f89e4085831e602ba234d
Anton Björkman <antonidag@hotmail.com>
2025-05-27 09:43:06 +0200

    fix faulty imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ApiDocs from "./pages/ApiDocs";
 import NotFound from "./pages/NotFound";
-import Testing from "./components/Testing";
 
 const queryClient = new QueryClient();
 
